075bbc0a5766780ba12c46d941c96c50947920fc
Documenter.jl <documenter@juliadocs.github.io>
2026-02-05 19:40:30 +0000

    build based on b427022

---
+++ v0.2.0/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = BenchmarkFlatnessbasedControl","category":"page"},{"location":"#BenchmarkFlatnessbasedControl","page":"Home","title":"BenchmarkFlatnessbasedControl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for BenchmarkFlatnessbasedControl.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [BenchmarkFlatnessbasedControl]","category":"page"}]
+}
